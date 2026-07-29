872ba5be6f734e87a206b9d1fe6cc39f7004d743
Dempsey845 <dempseynewton1@gmail.com>
2026-07-29 08:38:52 +0100

    Added a end delay to the spore hitbox so the player cant take damage while the effect is stopping.

--- Enemies/Puffcap/spore_hitbox.gd
+++ Enemies/Puffcap/spore_hitbox.gd
@@ -2,6 +2,7 @@ class_name SporeHitbox
 extends Hitbox
 
 @export var start_delay: float = 0.5
+@export var end_delay: float = -1.5
 
 
 @onready var spore_controller: SporeController = $SporeController
@@ -12,6 +13,6 @@ func _ready() -> void:
     enemy.attacked.connect(func(_target: Node3D):
         await get_tree().create_timer(start_delay).timeout
         spore_controller.start_tick_damage()
-        await get_tree().create_timer(enemy.attack_cooldown - start_delay).timeout
+        await get_tree().create_timer(enemy.attack_cooldown + end_delay).timeout
         spore_controller.stop_tick_damage()
     )
