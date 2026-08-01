b4311bff89a7dfebb3b6e66723c550899d94971c
Dempsey845 <dempseynewton1@gmail.com>
2026-08-01 20:32:17 +0100

    Adjusted the puffcap enemies attack values to make it feel more active.

--- Enemies/Puffcap/spore_hitbox.gd
+++ Enemies/Puffcap/spore_hitbox.gd
@@ -3,16 +3,17 @@ extends Hitbox
 
 @export var start_delay: float = 0.5
 @export var end_delay: float = -1.5
+@export var damage_duration: float = 5.0
 
 
 @onready var spore_controller: SporeController = $SporeController
 
 func _ready() -> void:
-    var enemy: Enemy = get_parent()
+	var enemy: Enemy = get_parent()
 
-    enemy.attacked.connect(func(_target: Node3D):
-        await get_tree().create_timer(start_delay).timeout
-        spore_controller.start_tick_damage()
-        await get_tree().create_timer(enemy.attack_cooldown + end_delay).timeout
-        spore_controller.stop_tick_damage()
-    )
+	enemy.attacked.connect(func(_target: Node3D):
+		await get_tree().create_timer(start_delay).timeout
+		spore_controller.start_tick_damage()
+		await get_tree().create_timer(damage_duration + end_delay).timeout
+		spore_controller.stop_tick_damage()
+	)
