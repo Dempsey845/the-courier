efdc1c469f5cd6c8fe5b58605f3c253da2dc951c
Dempsey845 <dempseynewton1@gmail.com>
2026-08-02 14:24:42 +0100

    Worked more on the dungeon area and created the transition.

---
+++ World/Areas/Ruins/dungeon_entrance_area.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+@onready var animation_player: AnimationPlayer = $"CanvasLayer/AnimationPlayer"
+
+var dungeon_scene: PackedScene = preload("uid://bvswcq67w3glb")
+
+func _ready() -> void:
+    body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node3D):
+    if body is Player:
+        animation_player.play("fade_out")
+        await animation_player.animation_finished
+        get_tree().change_scene_to_packed.call_deferred(dungeon_scene)
