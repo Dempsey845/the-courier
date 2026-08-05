d1b0d62f58801f08b3b926d7a7a8a052ad41e383
Dempsey845 <dempseynewton1@gmail.com>
2026-08-05 10:42:33 +0100

    Added an extra check to ensure that item collectibles cannot be collected twice on the same instance.

--- Systems/Items/item_collectible.gd
+++ Systems/Items/item_collectible.gd
@@ -8,6 +8,8 @@ extends Area3D
 
 var index: int = -1
 
+var collected: bool
+
 func _ready() -> void:
     body_entered.connect(_on_body_entered)
 
@@ -16,7 +18,8 @@ func _ready() -> void:
     index = WorldCollectibles.add_world_collectible(self, global_position)
 
 func _on_body_entered(body: Node3D):
-    if body is Player and collect_delay_timer.is_stopped():
+    if body is Player and collect_delay_timer.is_stopped() and !collected:
+        collected = true
         animation_player.play("collect")
         await animation_player.animation_finished
 
