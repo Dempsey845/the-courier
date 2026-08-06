55107fac8b6dbd069eee4a492018694dc6de83bb
Dempsey845 <dempseynewton1@gmail.com>
2026-08-06 15:50:11 +0100

    Imported the new frog boss and created the shield effect.

---
+++ Enemies/RealmBoss/frog_shield.gd
@@ -0,0 +1,35 @@
+extends MeshInstance3D
+
+@export var transition_duration: float = 1.5
+
+@onready var shield_material: ShaderMaterial = \
+	get_active_material(0) as ShaderMaterial
+
+
+func _ready() -> void:
+	show_shield()
+
+func show_shield() -> void:
+	var tween := create_tween()
+
+	tween.tween_method(
+		_set_shield_transition,
+		0.0,
+		1.0,
+		transition_duration
+	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+
+func hide_shield() -> void:
+	var tween := create_tween()
+
+	tween.tween_method(
+		_set_shield_transition,
+		1.0,
+		0.0,
+		transition_duration
+	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+
+
+func _set_shield_transition(value: float) -> void:
+	shield_material.set_shader_parameter("transition", value)
