7852ca0b27f63287859c7132690074d1c6b6c22f
Dempsey845 <dempseynewton1@gmail.com>
2026-08-21 18:17:14 +0100

    Added a killbox to the dungeon area.

--- Player/player_health_manager.gd
+++ Player/player_health_manager.gd
@@ -9,18 +9,18 @@ signal protection_ended
 @onready var protection_timer: Timer = $ProtectionTimer
 
 func _ready() -> void:
-    health.damage_taken.connect(_on_damage_taken)
+	health.damage_taken.connect(_on_damage_taken)
 
-    protection_timer.timeout.connect(func():
-        health.can_damage = true
-        protection_ended.emit()
-    )
+	protection_timer.timeout.connect(func():
+		health.can_damage = true
+		protection_ended.emit()
+	)
 
-    health.death.connect(func():
-        get_parent().stop_moving()
-    )
+	health.death.connect(func():
+		get_parent().stop_moving()
+	)
 
 func _on_damage_taken(_damage_amount, _new_health):
-    health.can_damage = false
-    protection_timer.start()
-    protection_started.emit()
+	health.can_damage = false
+	protection_timer.start()
+	protection_started.emit()
